f625b8e8f577c90191e850d82e178dafd2eb5c94
song1900 <woojin1900@gmail.com>
2021-05-11 00:05:00 +0900

    add viewmodel files

---
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoDetailViewModel.swift
+//  RxMemo
+//
+//  Created by 송우진 on 2021/05/10.
+//
+
+import Foundation
+
+class MemoDetailViewModel {
+    
+}
---
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoListViewModel.swift
+//  RxMemo
+//
+//  Created by 송우진 on 2021/05/10.
+//
+
+import Foundation
+
+class MemoListViewModel {
+    
+}
