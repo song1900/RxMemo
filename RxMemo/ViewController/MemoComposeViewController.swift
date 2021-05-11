17bfe9d37e84e463ed9f9535ed8a51ed1cd0a7f4
song1900 <woojin1900@gmail.com>
2021-05-11 16:49:12 +0900

    compose scene

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,20 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: MemoComposeViewModel!
 
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+    
 
     func bindViewModel() {
-         
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance) // double tap을 방지하기 위해 0.5초마다 tap을 처리
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+        
     }
+    
+    
 
 }
